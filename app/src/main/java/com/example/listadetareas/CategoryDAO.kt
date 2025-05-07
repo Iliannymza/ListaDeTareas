0e191fccdec22bf008a90f2bb8a6c4db4310db31
Iliannymza <mendoza71997@gmail.com>
2025-05-07 17:42:15 +0200

    se creo la DatabaseManager, las entidades y el DAO , se completo la primera clase, hacer push y terminar en casa.

--- app/src/main/java/com/example/listadetareas/CategoryDAO.kt
+++ app/src/main/java/com/example/listadetareas/CategoryDAO.kt
@@ -3,6 +3,7 @@ package com.example.listadetareas
 import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
 import android.util.Log
 
 class CategoryDAO(private val context: Context) {
@@ -25,7 +26,7 @@ class CategoryDAO(private val context: Context) {
          val values = ContentValues()
          values.put(Category.COLUMN_NAME_TITLE, category.title)
 
-    // Insert the new row, returning the primary key value of the new row
+        // Insert the new row, returning the primary key value of the new row
          val newRowId = db.insert(Category.TABLE_NAME, null, values)
 
          Log.i("DATABASE", "Inserted a category with id: $newRowId")
@@ -45,9 +46,10 @@ class CategoryDAO(private val context: Context) {
             val values = ContentValues()
             values.put(Category.COLUMN_NAME_TITLE, category.title)
 
-            // Which row to update, based on the title
+            // Which row to update, based on the id
             val selection = "${Category.COLUMN_NAME_ID} = ${category.id}"
 
+            // Update the new row, returning the primary key value of the new row
             val count = db.update(Category.TABLE_NAME, values, selection, null)
 
             Log.i("DATABASE", "Update category with id: ${category.id}")
@@ -63,18 +65,13 @@ class CategoryDAO(private val context: Context) {
             open()
 
             try {
-                // Create a new map of values, where column names are the keys
-                val values = ContentValues()
-                values.put(Category.COLUMN_NAME_TITLE, category.title)
+                // Define ´where´ part of query.
+            val selection = "${Category.COLUMN_NAME_ID}  ${category.id}"
 
-            // Define 'where' part of query.
-            val selection = "${FeedEntry.COLUMN_NAME_TITLE} LIKE ?"
-            // Specify arguments in placeholder order.
-            val selectionArgs = arrayOf("MyTitle")
-            // Issue SQL statement.
-            val deletedRows = db.delete(FeedEntry.TABLE_NAME, selection, selectionArgs)
+                // Issue SQL statement
+            val deletedRows = db.delete(Category.TABLE_NAME, selection, null)
 
-            Log.i("DATABASE", "Update category with id: ${category.id}")
+            Log.i("DATABASE", "Delete category with id: ${category.id}")
         } catch (e: Exception) {
             e.printStackTrace()
         } finally {
@@ -82,7 +79,86 @@ class CategoryDAO(private val context: Context) {
             }
         }
 
-    // Obtener registro por ID
+        // Obtener registro por ID
+        fun findById(id: Long): Category? {
+            open()
 
-    // Obtener todos los registros
+            var category: Category? = null
+            try {
+                val projection = arrayOf(
+                    Category.COLUMN_NAME_ID,
+                    Category.COLUMN_NAME_TITLE
+                )
+
+                // FILTER RESULTS WHERE "id" = category.id
+                val selection = "${Category.COLUMN_NAME_ID}  $id"
+
+                val cursor = db.query(
+                    Category.TABLE_NAME,
+                    projection,
+                    selection,
+                    null,
+                    null,
+                    null,
+                    null
+
+                )
+
+                if (cursor.moveToNext()) {
+                    val id = cursor.getLong(cursor.getColumnIndexOrThrow(Category.COLUMN_NAME_ID))
+                    val title = cursor.getString(cursor.getColumnIndexOrThrow(Category.COLUMN_NAME_TITLE))
+
+                    category = Category(id, title)
+
+                }
+            } catch (e: Exception) {
+                    e.printStackTrace()
+            } finally {
+                close()
+            }
+
+            return  category
+        }
+
+        // Obtener todos los registros
+        fun findAll(): List<Category> {open()
+
+            var categoryList: MutableList<Category> = mutableListOf()
+            try {
+                val projection = arrayOf(
+                    Category.COLUMN_NAME_ID,
+                    Category.COLUMN_NAME_TITLE
+                )
+
+                // FILTER RESULTS WHERE "id" = category.id
+                val selection = null
+
+                val cursor = db.query(
+                    Category.TABLE_NAME,
+                    projection,
+                    selection,
+                    null,
+                    null,
+                    null,
+                    null
+
+                )
+
+                while (cursor.moveToNext()) {
+                    val id = cursor.getLong(cursor.getColumnIndexOrThrow(Category.COLUMN_NAME_ID))
+                    val title = cursor.getString(cursor.getColumnIndexOrThrow(Category.COLUMN_NAME_TITLE))
+
+                   val category = Category(id, title)
+                    categoryList.add(category)
+
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                close()
+            }
+
+            return categoryList
+
+        }
 }
