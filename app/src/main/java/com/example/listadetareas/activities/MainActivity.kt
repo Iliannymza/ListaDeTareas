985779e94f0de6172b86aaeaef1ac3e2645b11b6
Iliannymza <mendoza71997@gmail.com>
2025-05-07 19:57:46 +0200

    continuar en la parte de del boton float

--- app/src/main/java/com/example/listadetareas/activities/MainActivity.kt
+++ app/src/main/java/com/example/listadetareas/activities/MainActivity.kt
@@ -5,17 +5,44 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.listadetareas.R
+import com.example.listadetareas.adapters.CategoryAdapter
+import com.example.listadetareas.data.Category
+import com.example.listadetareas.data.CategoryDAO
+import com.example.listadetareas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    lateinit var adapter: CategoryAdapter
+    var categoryList: List<Category> = emptyList()
+
+    lateinit var categoryDAO: CategoryDAO
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        categoryDAO = CategoryDAO(this)
+
+        categoryList = categoryDAO.findAll()
+
+        adapter = CategoryAdapter(categoryList) {
+            // he pulsado una categoria
+        }
+
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
---
+++ app/src/main/java/com/example/listadetareas/adapters/CategoryAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.listadetareas.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.listadetareas.data.Category
+
+class CategoryAdapter(
+    var items: List<Category>,
+    val onItemClick: (position: Int) -> Unit
+): Adapter<CategoryViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
+        val binding = ItemCategoryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CategoryViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
+        val category = items[position]
+        holder.render(category)
+        holder.itemView.setOnClickListener {
+            onItemClick(position)
+        }
+    }
+
+    fun updateItems(items: List<Category>) {
+        this.items = items
+        notifyDataSetChanged()
+    }
+}
+
+class  CategoryViewHolder(val binding: ItemCategoryBinding) : ViewHolder(binding.root) {
+
+    fun render(category: Category) {
+        binding.titleTextView.text = category.title
+    }
+}
